6c8d86bfb494887ee19a78673e2d60189e475fc7
johnnikolaou <giannid.nikolaou@gmail.com>
2023-05-06 12:49:21 +0300

    Implement POST logic for signupLogin

--- routes.js
+++ routes.js
@@ -10,4 +10,20 @@ router.get('/sign', (req, res) => {
     res.render('signupLogin', {layout: "sign"});
 })
 
+router.post("/login", async (req, res) => {
+    console.log(req.body['loginEmail']);
+    console.log(req.body['loginPassword']);
+    res.redirect('/');
+});
+
+router.post("/signup", async (req, res) => {
+    console.log(req.body['signUpEmail']);
+    console.log(req.body['signUpPassword']);
+    console.log(req.body['signUpFirstName']);
+    console.log(req.body['signUpLastName']);
+    console.log(req.body['signUpPhone']);
+    console.log(req.body['signUpCity']);
+    res.redirect('/');
+});
+
 export { router };
