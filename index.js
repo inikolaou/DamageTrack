c69127b4ff96b32df1d7cbb8b67de19a29e6c771
johnnikolaou <giannid.nikolaou@gmail.com>
2023-05-09 19:55:28 +0300

    Dockerize the application

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ import { engine } from 'express-handlebars';
 import { router } from './routes.js';
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost:27017/testDB', {
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/testDB";
+
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
